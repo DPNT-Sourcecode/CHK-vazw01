d2f56511865d1674b3a07e07ce4648e438bd9b0f
sbx_user1051 <sbx_user1051@169.254.2.213>
2025-11-07 12:00:44 +0000

    Fri Nov 07 12:00:44 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,7 +1,29 @@
+from collections import Counter
 
 class CheckoutSolution:
 
+    PRICES = {"A": 50, "B": 30, "C": 20, "D": 15}
+    OFFERS = {"A": (3, 130), "B" :(2,45)}
+
     # skus = unicode string
     def checkout(self, skus):
+        if not isinstance (skus, str) or not skus.isalpha():
+            return -1
+
+        counts = Counter(skus)
+        basket_total = 0
+
+        for item, count in counts:
+            if item not in self.PRICES:
+                return -1
+
+            offer_total = 0
+            if item in self.OFFERS:
+                offer_count, offer_price = self.OFFERS[item]
+                
+
+
+
+
 
 

fa92c05e437d8bd77813348bb4af7ee098637592
sbx_user1051 <sbx_user1051@169.254.32.193>
2025-11-10 17:13:30 +0000

    Mon Nov 10 17:13:30 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,10 +3,10 @@ from typing import Dict, List, Tuple
 
 PRICES: Dict[str, int] = {
     "A": 50, "B": 30, "C": 20, "D": 15, "E": 40, "F": 10,
-    "G": 20, "H": 10, "I": 35, "J": 60, "K": 80, "L": 90,
+    "G": 20, "H": 10, "I": 35, "J": 60, "K": 70, "L": 90,
     "M": 15, "N": 40, "O": 10, "P": 50, "Q": 30, "R": 50,
-    "S": 30, "T": 20, "U": 40, "V": 50, "W": 20, "X": 90,
-    "Y": 10, "Z": 50,
+    "S": 20, "T": 20, "U": 40, "V": 50, "W": 20, "X": 17,
+    "Y": 20, "Z": 21,
 }
 
 MULTI_BUY_OFFERS: Dict[str, list] = {
@@ -106,6 +106,3 @@ class CheckoutSolution:
                 basket_total += chargeable_count * unit_price
 
         return basket_total
-
-
-
